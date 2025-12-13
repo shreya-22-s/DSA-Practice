9a7f22e8c6c6d88cc0a11a87dcf10c613e923cf3
shreya-22-s <shreyasingh802302@gmail.com>
2025-12-13 17:58:07 +0530

    Implement method to remove adjacent duplicates from string

---
+++ Stack/Remove_all_adjacent_duplicate.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String removeDuplicates(String s) {
+        Stack<Character> st = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (!st.isEmpty() && st.peek() == ch) {
+                st.pop();          // remove duplicate
+            } else {
+                st.push(ch);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (char c : st) {
+            sb.append(c);
+        }
+
+        return sb.toString();
+    }
+}
