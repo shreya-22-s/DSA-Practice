ac3b66b9805233b3ab7ea2567743a11e3889bae0
shreya-22-s <shreyasingh802302@gmail.com>
2025-12-24 19:01:52 +0530

    Implement method to check divisible pairs in array

---
+++ HashMap/CheckifrrayPairsAreDivisibleByk.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean canArrange(int[] arr, int k) {
+        int[] mp=new int[k];
+        for(int num:arr){
+            int rem=(num%k+k)%k;
+            mp[rem]++;
+        }
+        if(mp[0]%2!=0)return false;
+        for(int rem=1;rem<=k/2;rem++){
+            int counter=k-rem;
+            if(mp[counter]!=mp[rem]){
+                return false;
+            }
+        }
+        return true; 
+    }
+}
